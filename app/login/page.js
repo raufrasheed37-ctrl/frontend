c3fc52161b5f510b7fe70a8c9c7821a00423aa37
raufrasheed37-ctrl <raufrasheed37@gmail.com>
2026-04-12 08:53:43 +0100

    Implement login functionality with API integration
    Refactor login component to include API call for authentication and handle responses.

--- app/login/page.js
+++ app/login/page.js
@@ -1,19 +1,11 @@
-"use client";
-
-import { useState } from "react";
-import Link from "next/link";
-import { z } from "zod";
-
-const loginSchema = z.object({
-  email: z.string().email("Invalid email"),
-  password: z.string().min(6, "Password must be at least 6 characters"),
-});
+import { useRouter } from "next/navigation";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const router = useRouter();
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const result = loginSchema.safeParse({ email, password });
@@ -23,38 +15,24 @@ export default function Login() {
       return;
     }
 
-    alert("Login ready (backend next)");
+    try {
+      const res = await fetch("https://https://auth-app-zg5k.onrender.com/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await res.json();
+
+      if (data.token) {
+        localStorage.setItem("token", data.token);
+        router.push("/dashboard");
+      } else {
+        alert(data.message || "Login failed");
+      }
+    } catch (error) {
+      alert("Something went wrong");
+    }
   };
-
-  return (
-    <div className="container">
-      <form className="card" onSubmit={handleSubmit}>
-        
-        <h2 className="title">Login</h2>
-
-        <input
-          className="input"
-          placeholder="Email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <input
-          type="password"
-          className="input"
-          placeholder="Password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-
-        <button className="button">Login</button>
-
-        <p style={{ textAlign: "center", marginTop: "10px" }}>
-  Don’t have an account?{" "}
-  <Link href="/register" className="link">
-    Register
-  </Link>
-</p>
-
-      </form>
-    </div>
-  );
-}
