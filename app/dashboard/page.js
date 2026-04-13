e4d112b59127c1ac83a617b3caf0fb21dda5a658
raufrasheed37-ctrl <raufrasheed37@gmail.com>
2026-04-13 13:45:07 +0100

    Refactor authentication handling in Dashboard

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -3,54 +3,48 @@
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import Navbar from "@/components/Navbar";
+import useAuthStore from "@/store/authStore";
+import api from "@/utils/api";
 
 export default function Dashboard() {
   const [user, setUser] = useState(null);
   const router = useRouter();
+  const { isAuthenticated } = useAuthStore();
 
   useEffect(() => {
-    const token = localStorage.getItem("token");
-
-    if (!token) {
+    if (!isAuthenticated) {
       router.push("/login");
       return;
     }
 
-    fetch("https://auth-app-zg5k.onrender.com/api/users/profile", {
-      headers: {
-        Authorization: `Bearer ${token}`,
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.user) {
-          setUser(data.user);
-        } else {
-          router.push("/login");
-        }
+    api.get("/users/profile")
+      .then((res) => {
+        setUser(res.data.user);
+      })
+      .catch(() => {
+        router.push("/login");
       });
-  }, []);
+
+  }, [isAuthenticated]);
 
   return (
-  <>
-    <Navbar />
+    <>
+      <Navbar />
 
-    <div className="dashboard-container">
-      <div className="dashboard-card">
-        
-        <h1 className="dashboard-title">Dashboard</h1>
+      <div className="dashboard-container">
+        <div className="dashboard-card">
+          <h1 className="dashboard-title">Dashboard</h1>
 
-        {user && (
-          <div className="user-box">
-            <p className="greeting">Welcome back 👋</p>
-            <p className="username">
-              {user.email.slice(0, 4).toUpperCase()}
-            </p>
-          </div>
-        )}
-
+          {user && (
+            <div className="user-box">
+              <p className="greeting">Welcome back 👋</p>
+              <p className="username">
+                {user.email.slice(0, 4).toUpperCase()}
+              </p>
+            </div>
+          )}
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
 }
