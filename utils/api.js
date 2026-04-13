65c83575f34fb68895ab9180390ed59ca78eb821
raufrasheed37-ctrl <raufrasheed37@gmail.com>
2026-04-13 13:42:53 +0100

    Change API base URL and add request interceptor
    Updated the base URL for the API and added a request interceptor to include the authorization token.

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,17 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api", // backend later
+  baseURL: "https://auth-app-zg5k.onrender.com/api",
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
+
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
